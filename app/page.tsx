bb9aa29c3d8414efca45137571f8fc2e1e0e2289
irshad-zlight <irshad.konnola@zlight.in>
2025-06-29 12:45:50 +0530

    project initial setup done

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+export default function Home() {
+ return(
+<>
+<h2>
+  Hey!
+</h2>
+<Button> Helllo guys</Button>
+</>
+ )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "oklch(var(--background) / <alpha-value>)",
+        foreground: "oklch(var(--foreground) / <alpha-value>)",
+        primary: "oklch(var(--primary) / <alpha-value>)",
+        "primary-foreground": "oklch(var(--primary-foreground) / <alpha-value>)",
+        secondary: "oklch(var(--secondary) / <alpha-value>)",
+        "secondary-foreground": "oklch(var(--secondary-foreground) / <alpha-value>)",
+        muted: "oklch(var(--muted) / <alpha-value>)",
+        "muted-foreground": "oklch(var(--muted-foreground) / <alpha-value>)",
+        accent: "oklch(var(--accent) / <alpha-value>)",
+        "accent-foreground": "oklch(var(--accent-foreground) / <alpha-value>)",
+        destructive: "oklch(var(--destructive) / <alpha-value>)",
+        input: "oklch(var(--input) / <alpha-value>)",
+        border: "oklch(var(--border) / <alpha-value>)",
+        ring: "oklch(var(--ring) / <alpha-value>)",
+        card: "oklch(var(--card) / <alpha-value>)",
+        "card-foreground": "oklch(var(--card-foreground) / <alpha-value>)",
+        popover: "oklch(var(--popover) / <alpha-value>)",
+        "popover-foreground": "oklch(var(--popover-foreground) / <alpha-value>)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [],
+};
